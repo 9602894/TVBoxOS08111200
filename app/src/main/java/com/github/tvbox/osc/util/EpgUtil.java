21480b36b9809cb99af97514fb65ac120cb3e094
9602894 <9602894@qq.com>
2026-08-15 19:11:03 +0800

    Add files via upload

--- app/src/main/java/com/github/tvbox/osc/util/EpgUtil.java
+++ app/src/main/java/com/github/tvbox/osc/util/EpgUtil.java
@@ -16,6 +16,7 @@ import com.google.gson.JsonParser;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Hashtable;
 import java.util.List;
 
