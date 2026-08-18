ccd1ca4156f3c31a7382532eb24ca6acbc36a8cb
9602894 <9602894@qq.com>
2026-08-18 15:12:28 +0800

    Add files via upload

--- app/src/main/java/com/github/tvbox/osc/ui/tv/CustomView/MusicLoadingView.java
+++ app/src/main/java/com/github/tvbox/osc/ui/tv/CustomView/MusicLoadingView.java
@@ -4,10 +4,6 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.FrameLayout;
 
-/**
- * 占位类：原文件中的 MarqueeTextView 已迁移到 MarqueeTextView.java
- * 如项目中无其他引用可安全删除此文件
- */
 class MusicLoadingView extends FrameLayout {
     public MusicLoadingView(Context context) { super(context); }
     public MusicLoadingView(Context context, AttributeSet attrs) { super(context, attrs); }
